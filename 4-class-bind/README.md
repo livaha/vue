869b485eb2dcf8b0b81fe5c5261917b3ed5a1563
sbwxffnhc <1192137406@qq.com>
2018-11-29 23:45:30 +0800

    class bind

---
+++ 4-class-bind/README.md
@@ -0,0 +1,22 @@
+webpack使用：
+
+命令： npm start  或 npm run server
+
+
+
+**先通过查找vue-loader官方文档了解vue文件的写法**
+
+#### vue单文件方式
+
+- 单文件就是以*.vue结尾的文件。最终通过webpack也会编译成*.js在浏览器运行
+- 内容： <template></template> + <script></script> + <style></style>
+  - 1:template中只能有一个根节点 2.x
+  - 2:script中  按照 export default {配置} 来写
+  - 3:style中 可以设置scoped属性，让其只在template中生效
+
+#### 以单文件的方式启动
+
+- webpack找人来理解你的单文件代码
+  - vue-loader,vue-template-compiler,代码中依赖vue,
+- 启动命令
+- `..\\node_modules\\.bin\\webpack-dev-server --inline --hot --open`
---
+++ 4-class-bind/src/app.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+    1.单个class:
+    <div v-bind:class="isRed?'red':'yellow'">1.单个class:</div>
+    2.多个class	:    
+    <div :class="{'red':true,'big':true}">2.多个class:</div>
+	3.复杂情况，通过遍历，根据当前对象的成绩，匹配成绩和样式的清单对象，用成绩做key，取对象的value，最终返回字符串做样式名
+    <ul>
+    	<li v-for="stu in stus" :class="{ 'A':'red','B':'yellow'}[stu.score]">
+    		{{stu.name}}
+    	</li>
+    </ul>
+    </div>
+</template>
+<script>
+// class 取一个 返回一个字符串
+// 取多个  返回一个对象
+// 一个学生列表  每个人都有成绩 AB，根据当前学生的成绩匹配显示不同的颜色
+// A-red B-green
+    export default {
+        data(){
+            return {
+            	isRed:true,
+            	stus:[{name:'jack',score:'A'},{name:'rose',score:'B'}]
+
+            }
+        }
+    }
+</script>
+<style >
+.red{
+	background-color:red;
+}
+.yellow{
+	background-color:yellow;
+}
+.big{
+    font-size: 30px;
+}
+
+</style>
