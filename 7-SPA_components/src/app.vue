b5dfef60f84615d77ea6a0f578ac81b040ff0a94
sbwxffnhc <1192137406@qq.com>
2018-11-30 16:05:47 +0800

    SPA compnents

---
+++ 7-SPA_components/src/app.vue
@@ -0,0 +1,30 @@
+<template>
+   <div>
+      <header-vue></header-vue>
+      <body-vue></body-vue>
+      <footer-vue></footer-vue>
+   </div>
+</template>
+<script>
+  //引入子组件对象
+  import headerVue from './components/header.vue'
+  import bodyVue from './components/body.vue'
+  import footerVue from './components/footer.vue'
+
+  export default{
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      //组件名（在模板中使用）：组件对象
+      headerVue : headerVue,
+      bodyVue : bodyVue,
+      footerVue   //两相同可简写成一个
+    }
+  }
+</script>
+<style >
+
+</style>
---
+++ 7-SPA_components/src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		Footer
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+
+			}
+		}
+	}
+</script>
+<style scoped>
+ div{
+    height: 100px;
+    background-color: hotpink;
+ }
+</style>
